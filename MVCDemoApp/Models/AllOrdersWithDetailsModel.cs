846a974fad1c6a00565aad5c3e75ca56a7d2942e
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-02-12 18:48:50 +0100

    Model for storing a lost of orders with food details

---
+++ MVCDemoApp/Models/AllOrdersWithDetailsModel.cs
@@ -0,0 +1,13 @@
+﻿using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDemoApp.Models
+{
+    public class AllOrdersWithDetailsModel
+    {
+        public List<OrderModelWithFoodDetails> Orders { get; set; } = new List<OrderModelWithFoodDetails>();
+    }
+}
